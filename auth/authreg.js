bc97f0fc7145f9ca9747bc5e2891504adb6d8cab
vikram <srivikram02@gmail.com>
2023-03-23 16:24:14 +0530

    5

--- auth/authreg.js
+++ auth/authreg.js
@@ -1,12 +1,24 @@
+const createerror = require('http-errors');
 const User = require("../model/auth_reg");
 const bcrypt = require("bcryptjs");
 
 
 
 const register = async (req, res) => {
+  
   const { username, userpassword, useremail,userphno,dateofbirth } = req.body;
-  console.log(userphno); 
- 
+  const doesexist = await User.findOne({useremail: useremail});
+        if(doesexist){res.send("email already exist")
+         return
+        console.log("ddddd")}
+      //throw createerror.Conflict(`${useremail} already exists`);
+
+  const phnodoesexist = await User.findOne({userphno: userphno});
+     if(phnodoesexist){res.send("phnumber already exist")
+    return
+  }
+      //throw createerror.Conflict(`${userphno} already exists`);
+
   bcrypt.hash(userpassword, 10).then(
     async (hash) =>
       await User.create({
@@ -16,14 +28,20 @@ const register = async (req, res) => {
         userphno,
         dateofbirth,
         date:new Date().toLocaleDateString('es-MX'),
-
       })
+    
+    
+      
+
         .then((user) =>
           res.status(200).json({
             message: "User successfully created",
             user,
+            
           })
+        
         )
+        
         .catch((error) =>
           res.status(400).json({
             message: "User not successful created",
